e8988944a33a234db028fcad7fb1d42786883892
Alejandro Martinez <j.alejandro.mrtnz@gmail.com>
2019-05-28 18:57:34 -0700

    finish exercise

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 var fs = require('fs');
+
+const files = fs.readdirSync('./challenge6/', 'utf-8');
+
+for (let file of files) {
+    let ext = file.slice(-3, file.length);
+    if (ext === 'txt') {
+        console.log(fs.readFileSync(`./challenge6/${file}`, 'utf-8'));
+    }
+}
